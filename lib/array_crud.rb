d23846e3e7a68e4dfc0da673c5836a799f8b67fd
Jake Kwak <jakejaekwak@gmail.com>
2020-05-11 21:22:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  array = [1,2,3,'four']
+  array.push('five')
 end
 
 def add_element_to_start_of_array(array, element)
